c2a74fb8cc384ea913f97b4521c35b24d08b0814
Stuart Smith <stuartsayswhat@googlemail.com>
2021-03-20 19:46:31 +0000

    updated designing underground

--- Questions/General/misc.py
+++ Questions/General/misc.py
@@ -18,3 +18,27 @@ print(cars)
 
 # removing id from dict whether or not it exsits
 dict.pop(id, None)
+
+
+# default dict
+from collections import defaultdict
+s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+d = defaultdict(list)
+for k, v in s:
+    d[k].append(v)
+    
+# set default_factory (default dict) to int
+s = 'mississippi'
+d = defaultdict(int)
+for k in s:
+    d[k] += 1
+
+sorted(d.items())
+
+# set default_factory to set
+s = [('red', 1), ('blue', 2), ('red', 3), ('blue', 4), ('red', 1), ('blue', 4)]
+d = defaultdict(set)
+for k, v in s:
+    d[k].add(v)
+
+sorted(d.items())
