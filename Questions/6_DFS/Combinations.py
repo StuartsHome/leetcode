1239763ffbe8a2305f3ad1db1f2a29efa3bcf662
Stuart Smith <stuartsayswhat@googlemail.com>
2021-05-06 19:15:25 +0100

    combos

---
+++ Questions/6_DFS/Combinations.py
@@ -0,0 +1,21 @@
+# Leetcode 77. Combinations
+# Given two integers n and k, return all possible combinations of
+# k numbers out of the range [1, n].
+
+
+class Solution:
+    def combine(self, n, k):
+        nums = [x for x in range(1, n + 1)]
+        
+        result = []
+        def dfs(ind, path):
+            if len(path) == k and path not in result:
+                result.append(path)
+                return
+            for i in range(len(ind)):
+                dfs(ind[i + 1:], path + [ind[i]])
+        
+        dfs(nums, [])
+        return result
+Run = Solution()
+Run.combine(4,2)
--- Questions/Knapsack_Problems/Jump_Game.py
+++ Questions/Knapsack_Problems/Jump_Game.py
@@ -2,6 +2,7 @@
 
 class Solution:
     def canJump(self, nums):
+        # Similar to Jump Game II
         curr_max = 0
         for i in range(len(nums)):
             if curr_max < i:
@@ -10,26 +11,29 @@ class Solution:
                 return True
             curr_max = max(curr_max, i + nums[i])
 
+        #Reverse
+        curr_max = len(nums) - 1                # Iterate backwards from second to last item until the first item
+        for i in range(len(nums)-2, -1, -1):    # If this index has jump count which can reach to or beyond the last position
+            if i + nums[i] >= curr_max:          # We just need to reach this new index
+                curr_max = i
+        return curr_max == 0
+
+        # DP
+        N = len(nums)
+        dp = [0] * N
+        dp[0] = nums[0]
+        
+        for i in range(1, N - 1):
+            if dp[i - 1] < i:
+                return False
+            dp[i] = max(i + nums[i], dp[i - 1])
+            if dp[i] >= N - 1:
+                return True
+        return dp[N - 2] >= N - 1
 
 Run = Solution()
-Run.canJump([2,0])
+Run.canJump([2,3,1,1,4])
+([2,0])
 ([1])
 ([2,5,0,0])
-([2,3,1,1,4])
 
-
-        # result = 0
-        # curr_max = nums[0]
-        # total_max = 0
-        # i = 0
-        # # for i in range(1, len(nums)):
-        # while True:
-        #     if i >= len(nums):
-        #         return False
-        #     if i == len(nums) -1:
-        #         return True
-        #     if i >= curr_max:
-        #         if nums[i] == 0 and i < len(nums):
-        #             return False
-        #         curr_max = nums[i]
-        #     i += 1
