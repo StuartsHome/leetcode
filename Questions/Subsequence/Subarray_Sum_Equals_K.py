8b9a362f94574da7860bf7a0765c63505ea004a8
Stuart Smith <stuartsayswhat@googlemail.com>
2021-05-13 09:32:23 +0100

    hmm

--- Questions/Subsequence/Subarray_Sum_Equals_K.py
+++ Questions/Subsequence/Subarray_Sum_Equals_K.py
@@ -1,16 +1,41 @@
 # Leetcode 560. Subarray Sum Equals K
-
+import collections
 class Solution:
     def subarraySum(self, nums, k):
+        running_sum = 0
+        hash_table = collections.defaultdict(lambda:0)
+        total = 0
+        for x in nums:
+            running_sum += x
+            sum = running_sum - k
+            if sum in hash_table:
+                total += hash_table[sum]
+            if running_sum == k:
+                total += 1
+            hash_table[running_sum] += 1
+        return total
+Run = Solution()
+Run.subarraySum([1,2,2,0,3,2,5], 5)
+[0, 1, 3, 5, 5, 8, 10, 15]
+
+        # TLE
+"""
+        # TLE - using accumalation table
+        # Create accumulation table
+        # Length + 1 for accumulation to work
         count = 0
-        total = [0] * (len(nums) + 1)
-        for i in range(1, len(total)):
-            total[i] = total[i - 1] + nums[i - 1]
+        memo = [0] * (len(nums) + 1)
+        for i in range(1, len(memo)):
+            # print(memo[i-1], nums[i -1])
+            memo[i] = memo[i - 1] + nums[i - 1]
+        # Now, you have your accumalation table: memo
+        # From index j (curr) see if memo[j] - memo[x] == k
         for j in range(len(nums)):
             for x in range(j + 1, len(nums) + 1):
-                if total[x] - total[j] == k:
+                print(j, x, memo[x], memo[j])
+                if memo[x] - memo[j] == k:
                     count += 1
-        print(total, count)
+        """
 
         # TLE - O(N^3)
 """        result = 0
@@ -25,7 +50,8 @@ class Solution:
         print(result)"""
             
 
-Run = Solution()
-Run.subarraySum([1,-1,0],0)
-([1,2,3], 3)
-([1,1,1], 2)
+# Run = Solution()
+# Run.subarraySum([1,2,2,0,3,2,5], 5)
+# ([1,-1,0],0)
+# ([1,2,3], 3)
+# ([1,1,1], 2)
