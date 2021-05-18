cfcaca8118fad44a359b61e908dfb4da3df38042
Stuart Smith <stuartsayswhat@googlemail.com>
2021-05-18 19:39:13 +0100

    frequency counter

--- Questions/Subsequence/Longest_Substring_K_Repeating_Chars.py
+++ Questions/Subsequence/Longest_Substring_K_Repeating_Chars.py
@@ -1,46 +1,98 @@
 # Leetcode 395. Longest Substring with At Least K Repeating Characters
+# Good video: https://www.youtube.com/watch?v=bHZkCAcj3dc&ab_channel=KnowledgeCenter
+# Good explanation: https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/discuss/753840/Python-Solution-from-Youtube-tutorial-Not-a-medium-question-for-sure-For-Interviews
+
+# Create count > n
+# Linear scan > n
+# Space > O(N)
+
+
+from collections import defaultdict
 class Solution:
     def longestSubstring(self, s, k):
+        # Recursion - Understandable
+        if k > len(s) or len(s) == 0:
+            return 0
+        if k <= 1:
+            return len(s)
         memo = {}
-        result = {}
-        for x in s:
-            if x in memo:
-                memo[x] += 1
-            else:
-                memo[x] = 1
-        result = {x:k for x,i in memo.items() if i >= k}
-        copy = result.copy()
-        sub = ""
-        total = 0
-        for i in s:
-            if i in copy:
-                copy[i] -= 1
-                sub += i
-                if sum(copy.values()) == 0:
-                    total = max(total, len(sub))
-            else:
-                sub = ""
-                copy = result.copy()
-        # return total
-        print(total)
+        for char in s:
+            memo[char] = memo.get(char, 0) + 1
         
+        left = 0
+        while left < len(s) and memo[s[left]] >= k:
+            left += 1
+        if left >= len(s) - 1:
+            return left
+        
+        substringLeft = self.longestSubstring(s[:left], k)
 
+        while left < len(s) and memo[s[left]] < k:
+            left += 1
+        
+        substringRight = self.longestSubstring(s[left:len(s)], k) if left < len(s) else 0
+        return max(substringLeft, substringRight)
+
+        # Brute Force - Really Slow
+        """
+        def build(start, s, k):
+            memo, valid, last = {}, set([]), -1
+            for i in range(start, len(s)):
+                memo[s[i]] = memo.get(s[i], 0) + 1
+                if memo[s[i]] and memo[s[i]] >= k:
+                    valid.add(s[i])
+                if len(memo) == len(valid):
+                    last = i
+
+            return 0 if last == -1 else last-start+1
+
+        total = 0
+        for j in range(len(s)):
+            total = max(total, build(j, s, k))
+        return total
+        """
+
+        # Brute Force - using Default Dict
+        """
+        def build(start, s, k):
+            fmap, valid, last = defaultdict(int), set([]), -1
+            for i in range(start, len(s)):
+                fmap[s[i]] += 1
+                if fmap[s[i]] >= k:
+                    valid.add(s[i])
+                if len(fmap) == len(valid):
+                    last = i
+            return 0 if last == -1 else last-start+1
+
+        total = 0
+        for i in range(len(s)):
+            total = max(total, build(i, s, k))
+        print(total)
+        """
 
 Run = Solution()
 Run.longestSubstring("bbaaacbd",3)
 ("ababacb", 3)
 ("aaabb", 3)
 
-
+# Recursion
 """
-        if len(s) < k: return 0
-        c = Counter(s)
-        st = 0
-        for p, v in enumerate(s):
+if len(s) < k: return 0
+c = Counter(s)
+st = 0
+for p, v in enumerate(s):
 
-            if c[v] < k:
+    if c[v] < k:
 
-                return max(self.longestSubstring(s[st:p],k), self.longestSubstring(s[p+1:],k))
+        return max(self.longestSubstring(s[st:p],k), self.longestSubstring(s[p+1:],k))
 
-        return len(s)
+return len(s)
+"""     
+
+
+# Frequency Counter
+# Dictionary instead of Counter
+"""
+for char in s:
+    hashmap[char] = hashmap.get(char, 0) + 1
 """
