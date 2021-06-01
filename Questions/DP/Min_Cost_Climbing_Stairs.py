0fbe87bcda81d2d6c19b937522bae1d920b743cc
Stuart Smith <stuartsayswhat@googlemail.com>
2021-06-01 21:29:47 +0100

    This is a test

--- Questions/DP/Min_Cost_Climbing_Stairs.py
+++ Questions/DP/Min_Cost_Climbing_Stairs.py
@@ -1,7 +1,14 @@
 # Leetcode 746. Min Cost Climbing Stairs
 
+# Solution 1 is:
+# T: O(N)
+# S: O(N)
+
+# Space can be reduced to O(1) Solution 2
+
 class Solution:
     def minCostClimbingStairs(self, cost):
+        # Solution 1. DP Bottom Up
         if len(cost) <= 2:
             return min(cost)
         dp = [0] * len(cost) 
@@ -13,6 +20,15 @@ class Solution:
             dp[i] = cost[i] + min(dp[i - 2], dp[i - 1])        
 
         print(dp)
+
+        # Solution 2. 
+        N = len(cost)
+        if N == 0 or N == 1:
+            return 0
+        first, second = cost[0], cost[1]
+        for i in range(2, N):
+            first, second = second, cost[i] + min(first, second)
+        return min(first, second)
 
 Run = Solution()
 Run.minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
---
+++ Questions/DP/README.md
@@ -0,0 +1,13 @@
+## DP
+
+
+### Bottom-up
+
+
+### Top-down
+Top-down dynamic programming starts at the top and works its way down to the base cases. Typically, this is implemented through recursion, and then made efficient using memoization.
+
+
+```python
+
+```
