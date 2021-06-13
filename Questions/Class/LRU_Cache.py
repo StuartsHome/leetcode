608f34a104eff7313274bbfe787fa3d013a74eb6
Stuart Smith <stuartsayswhat@googlemail.com>
2021-06-13 12:44:01 +0100

    LRU:

--- Questions/Class/LRU_Cache.py
+++ Questions/Class/LRU_Cache.py
@@ -1,92 +1,99 @@
-# Leetcode 146. LRU Cache
+# class cache:
+#     def __init__(self, capacity):
+#         self.memo = {}
+#         self.capacity = capacity
+#     def insert(self, key, val):
+#         curr = self.memo
+#         if key in curr:
+#             del curr[key]
+#             print(curr)
+#             curr[key] = val # updates original val to new val
+#             print(curr)
+#         else:
+#             curr[key] = val
+#             print(curr)
 
-# 1. Create Dummy node, and set head and tail to dummy.next
-# 2. Create LL methods: 
-# 2.1. - Remove head
-# 2.2. - Append new node
-# 2.3. - Unlink curr node
-
-# Tail - MRU
-# Head - LRU
-
-# When a get request is made for value already in cache, that value needs to be unlinked
-# and repopulated as newest element in LL as this was the latest element requested.
-
-# Get - if key is in the hash map ("cache hit"), find the corresponding LL node,
-# unlink the node from curr position and move to tail of the list
-# If cache miss, load into cache
-
-# Remove LRU - if cache is full, we evict the LRU cache item - it's at the head of the LL.
+#     def get(self, key):
+#         curr = self.memo
+#         try:
+#             temp = curr[key]
+#             print(temp)
+#         except KeyError as e:
+#             print("key not in cache", e) 
 
 class LinkedNode:
-    def __init__(self, val, key):
-        self.val = val
+    def __init__(self, key, val):
         self.key = key
+        self.val = val
         self.next = None
-        self.prev = None
+        self.prev = prev
 
-class LRUCache:
+class cache:
     def __init__(self, capacity):
-        self.capacity = capacity
         self.memo = {}
-        # create LL Node
-        self.dummy = LinkedNode(0, 0)
+        self.capacity = capacity
+        self.dummy = LinkedNode(0,0)
         self.head = self.dummy.next
         self.tail = self.dummy.next
-        
-    def remove_head_node(self):
-        if self.head is None:
-            return
-        prev = self.head
-        self.head = self.head.next
-        if self.head:
-            self.head.prev = None
-        del prev
-        
-    def append_new_node(self, new_node):
-        if self.tail is None:
-            self.head = self.tail = new_node
+
+    def removeHeadNode(self):
+        if self.head is None:               # Check if head is empty
+            return  
+        else:                       
+            temp = self.head
+            self.head = self.head.next      # Nothing to the left, but possibly something to the right
+            if self.head:
+                self.head.prev = None       # If head, make the head left == None
+            del temp                        # Delete old head
+
+
+    def addNodeLL(self, node):
+        if self.tail is None:                   # If tail is empty, it means head is empty as well becaue first node appended is head and tail
+            self.head = self.tail = node
         else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = self.tail.next #after linking new node to LL, the new_node now bbecomes tail node or (MRU) node
-            
-    def unlink_curr_node(self, node):
-        if self.head is node:
-            self.head = node.next
-            if node.next:
-                node.next.prev = None
-            return
-        prev, nex = node.prev, node.next # the next and prev links either side of the node to unlink
-        prev.next = nex
-        nex.prev = prev 
-        
+            self.tail.next = node               # Add to tail
+            node.prev = self.tail               # Double LL, so link both directions
+            self.tail = self.tail.next          # After linking new node to LL, the new_node now bbecomes tail node or (MRU) node
+
+    def unlinkNode(self, node):                 # Removes node from LL
+        if node is self.head:                   # If node == head:
+            self.head = node.next               # 1. Make self.head next node in LL
+            if node.next:                       # If that worked
+                node.next.prev = None           # 2. Remove the curr head
+        else:
+            prev, next = node.prev, node.next   # Else, the left (next) and right (prev) of node to remove   
+            prev.next = next                    # Left == Right
+            next.prev = prev                    # and Right == Left, essentially removing the node in the middle
+
     def get(self, key):
-        if key not in self.memo:
-            return -1
-        node = self.memo[key]
-        if node != self.tail:
-            self.unlink_curr_node(node)
-            self.append_new_node(node)
+        curr = self.memo
+        if key not in curr:                     # 1. Check if in memo, if not return
+            return "Key not in cache"
+        node = curr[key]
+        if node != self.tail:                   # 2. Check if node == tail, if yes return value, as no need to unlink from queue
+            self.unlinkNode(node)
+            self.addNodeLL(node)
+        print(node.val)
         return node.val
-        
-    def put(self, key, value):
-        if key in self.memo:
-            self.memo[key].val = value
+
+    def put(self, key, val):        # Insert
+        curr = self.memo            
+        if key in curr:                     # 1. Check if key in cache, if yes, update value and call get
+            curr[key].val = val     
             self.get(key)
             return
-        if len(self.memo) == self.capacity:
-            self.memo.pop(self.head.key)
-            self.remove_head_node()
-        new_node = LinkedNode(value, key)
-        self.memo[key] = new_node
-        self.append_new_node(new_node)
-        
-obj = LRUCache(2)
-obj.put(1, 1)
-obj.put(2,2)
-param_3 = obj.get(1)
-print(param_3)
-obj.put(3,3)
-param_3 = obj.get(2)
-print(param_3)
+        else:
+            if len(self.memo) == self.capacity:  # If at capacity remove head from 1. Cache, 2. LL
+                del curr[self.head.key]     # Remove old head from cache
+                self.removeHeadNode()        # Remove old head from LL
+            node = LinkedNode(key, val)     # Now create new node to add to 1. Cache, 2. LL
+            curr[key] = node                # 1. Cache
+            self.addNodeLL(node)            # 2. Add to LL
+
+
+Run = cache(1)
+Run.put(2, 1)
+Run.get(2)
+# Run.put("a", 100)
+# Run.put("a", 200)
+# Run.get(1000)
---
+++ Questions/Class/LRU_Cache_2.py
@@ -0,0 +1,92 @@
+# Leetcode 146. LRU Cache
+
+# 1. Create Dummy node, and set head and tail to dummy.next
+# 2. Create LL methods: 
+# 2.1. - Remove head
+# 2.2. - Append new node
+# 2.3. - Unlink curr node
+
+# Tail - MRU
+# Head - LRU
+
+# When a get request is made for value already in cache, that value needs to be unlinked
+# and repopulated as newest element in LL as this was the latest element requested.
+
+# Get - if key is in the hash map ("cache hit"), find the corresponding LL node,
+# unlink the node from curr position and move to tail of the list
+# If cache miss, load into cache
+
+# Remove LRU - if cache is full, we evict the LRU cache item - it's at the head of the LL.
+
+class LinkedNode:
+    def __init__(self, val, key):
+        self.val = val
+        self.key = key
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memo = {}
+        # create LL Node
+        self.dummy = LinkedNode(0, 0)
+        self.head = self.dummy.next
+        self.tail = self.dummy.next
+        
+    def remove_head_node(self):
+        if self.head is None:
+            return
+        prev = self.head
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+        del prev
+        
+    def append_new_node(self, new_node):
+        if self.tail is None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = self.tail.next #after linking new node to LL, the new_node now bbecomes tail node or (MRU) node
+            
+    def unlink_curr_node(self, node):
+        if self.head is node:
+            self.head = node.next
+            if node.next:
+                node.next.prev = None
+            return
+        prev, nex = node.prev, node.next # the next and prev links either side of the node to unlink
+        prev.next = nex
+        nex.prev = prev 
+        
+    def get(self, key):
+        if key not in self.memo:                # 1. Check if in memo, if not return
+            return -1
+        node = self.memo[key]
+        if node != self.tail:                   # 2. Check if node == tail, if yes return value, as no need to unlink from queue
+            self.unlink_curr_node(node)
+            self.append_new_node(node)
+        return node.val
+        
+    def put(self, key, value):
+        if key in self.memo:                    # 1. Check if key in cache, if yes, update value and call get
+            self.memo[key].val = value
+            self.get(key)
+            return
+        if len(self.memo) == self.capacity:
+            self.memo.pop(self.head.key)        # 2. If at capacity, remove head key from cache (not the new key)
+            self.remove_head_node()             #   Remove head node (oldest)
+        new_node = LinkedNode(value, key)
+        self.memo[key] = new_node
+        self.append_new_node(new_node)
+        
+obj = LRUCache(2)
+obj.put(1, 1)
+obj.put(2,2)
+param_3 = obj.get(1)
+print(param_3)
+obj.put(3,3)
+param_3 = obj.get(2)
+print(param_3)
