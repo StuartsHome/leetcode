70ef426439593d5b6ad15209a92238cb6495a22b
Stuart Smith <stuartsayswhat@googlemail.com>
2021-06-19 18:44:16 +0100

    more fenwick

--- Questions/Binary_Search/Range_Sum_Query_Mutable.py
+++ Questions/Binary_Search/Range_Sum_Query_Mutable.py
@@ -14,9 +14,11 @@
         # We need additional sqrt(n) memory to store all block sums.
 # 3. Binary Indexed Tree (Fenwick Tree)
     # Time Complexity:
-        # Preprocessing     - O(n)
-        # Range Sum Query   - O(log n)
+        # Preprocessing     - O(n log n)  - n elements and for every element takes log n to update itself and nodes to right
+        # Range Sum Query   - O(log n)  - The time taken will be the height of the Binary Indexed Tree, which at worst case will be O(log(n))
         # Update            - O(log n)
+    # Space:
+        # O(n)
 
 # Sqrt decompostion
 # 1. Split the array into blocks w/ length of sqrt(n)
@@ -26,6 +28,7 @@
 
 from math import ceil, sqrt
 class NumArray:
+    # Sqrt Decomposition
     # def __init__(self, nums):
     #     self.nums = nums
     #     self.n = ceil(sqrt(len(nums)))              # The number of buckets
@@ -51,36 +54,61 @@ class NumArray:
     #             ans += self.nums[k]
     #     return ans
 
-    # Binary Indexed Tree
+    # Binary Indexed Tree - Fenwick Tree
     def __init__(self, nums):
         self.dp = [0 for _ in range(len(nums) +1)]
         for key, val in enumerate(nums):
             self.increment(key, val)
     
-    def increment(self, i, val):
-        i += 1
-        while i < len(self.dp):
+    def increment(self, i, val):        # Used for filling self.dp in O(log n) and go back up the tree - O(log n)
+        i += 1                          # Given index i, the next node on the access path back up to the root in which we go right
+        while i < len(self.dp):         # is given by taking the binary representation of i and replacing the last 1 w/ a "0".
             self.dp[i] += val
-            i += i & -i                 # go to a larger range, which contains current range
+            i += i & -i                 # does two's complement and add's to original index
 
-    def prefix_sum(self, i):
-        i += 1
-        total = 0
-        while i > 0:
-            total += self.dp[i]
-            i -= i & -i                 # got to a new range, which is immediately before current range
+    def _prefix_sum(self, i):           # Get parent - Traverse up the tree from index i
+        i += 1                          # Uses bitwise "and" - the bit pattern is an intersection of the operator’s arguments.
+        total = 0                       
+        while i > 0:                    
+            total += self.dp[i]         
+            i -= i & -i                 # flips the least significant bit
         return total
     
-    def update(self, i, val):
-        delta = val - self.sumRange(i, i)
-        self.increment(i, delta)
+    def update(self, i, val):               
+        delta = val - self.sumRange(i, i)   # find the difference between curr val at index and val to update
+        self.increment(i, delta)            # Add that difference to every left node up the tree
 
     def sumRange(self, i, j):
-        return self.prefix_sum(j) - self.prefix_sum(i - 1)  
+        return self._prefix_sum(j) - self._prefix_sum(i - 1)  
 
 obj = NumArray([1,3,5, 10, 20, 32, 35, 40, 50, 65, 66, 67 ,100])
 obj.sumRange(3, 12)
 obj.update(1, 2)
+
+""" 
+# To get parent node in O(1):
+    # Start with index of node to find parent
+    # Increment index by 1 because self.dp is zero based
+    # Get current index binary
+    # Get current index 2's complement binary
+    # AND the two binaries to find difference
+    # Subtract the difference from the original index binary to find the next parent index
+# To get parent node and add values as traverse up O(1):
+    # The same as get parent node above +
+    # initialise a total at the beginning
+    # store self.dp[index] in total
+    # every time index is updated to move to new parent, update total
+# To fill out self.dp in O(n log n):
+    # for loop over self.dp
+    # increase i by 1 because we're zero index based
+    # Add value to self.dp[i]
+    # Now we need to find the next index to update by val
+    # Get two's complement of current index
+    # AND 2's complement index with original index to find difference
+    # Add the difference to original index to find new index
+"""
+
+
 
 # Original Implementation w/generators
 # class NumArray:
@@ -142,6 +170,57 @@ One way that we can think about improving this operation would be to change what
 Rather than storing the cumulative frequency up to the given point, you can instead think of just storing
 the amount that the current frequency has increased relative to the previous bucket. 
 
+# Implementation
+# 1. Length of tree will be len(n) + 1, because "0" (the root node) is a dummy node
+# 2. For every binary number, find the very last 1 in the number, then drop that bit off, along w/all the bits
+# that come after it.
+# 3. "0" = Left
+# 4. "1" = Right
+# 5. Lookup and update operations depend on the access path from the node back up to the root.
+# 6. Lookup - only care about the right links we follow
+#    Upate - only care about the left links we follow
+
+# To compute the cumulative sum up to a node:
+# 1. Write out node n in binary
+# 2. Set the counter to 0
+# 3. Repeat the following while n != 0
+    # 1. Add in the value at node n
+    # 2. Clear the rightmost 1 bit from n
+
+# 1. Follow path back-up to root.
+# 2. Update all nodes where we followed a left link upward:
+    # 1. Write out node n in binary
+    # 2. Set the counter to 0
+    # 3. Repeat the following while n != 0
+    # 4. Add in the value at node n
+    # 5. Clear the rightmost 0 bit from n
+
+
+"""
+
+"""
+0 = dummy node (root node), All ranges starting from 0 are new subtrees from dummy node(root)
+index 1 = 0 + 2^0               = range = 0 - 1
+index 2 = 0 + 2^1               = range = 0 - 2
+index 3 = 2^1 + 2^0             = range = 2 - 3
+index 4 = 0 + 2^2               = range = 0 - 4
+index 5 = 2^2 + 2^0             = range = 4 - 5
+index 6 = 2^2 + 2^1             = range = 4 - 6 
+index 7 = 2^2 + 2^1 + 2^0       = range = 6 - 7 (start from 2^2 + 2^1 = 6)
+index 8 = 0 + 2^3               = range = 0 - 8
+index 9 = 2^3 + 2^0             = range = 8 - 9 
+index 10 = 2^3 + 2^1            = range = 8 - 10 
+index 11 = 2^3 + 2^1 + 2^0      = range = 10 - 11 (start from 2^3 + 2^1 = 10) 
+
+
+
+"""
+
+"""
+2's complement
+7 = 111
+2's complement of 7 = flip all bits and add 1
+= 00 + 1 = 001
 
 
 """
---
+++ Questions/Binary_Search/Untitled-1.py
@@ -0,0 +1,7 @@
+
+
+aa = bin(10)
+bb = 100 
+print(bin(-10))
+bb -= 10 & -10
+print(bb)
